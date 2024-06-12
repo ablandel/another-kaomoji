2df272524b614493d65f8243b972a9226326391e
ablandel <github@ablandel.email>
2024-06-12 22:13:55 +0200

    feat: add kaomojis mappedBy association in the Tag table (to Kaomoji table)
    Signed-off-by: ablandel <github@ablandel.email>

--- src/main/kotlin/github/ablandel/anotherkaomoji/entity/Tag.kt
+++ src/main/kotlin/github/ablandel/anotherkaomoji/entity/Tag.kt
@@ -1,6 +1,7 @@
 package github.ablandel.anotherkaomoji.entity
 
 import jakarta.persistence.Entity
+import jakarta.persistence.ManyToMany
 import java.time.Instant
 
 @Entity(name = "tag")
@@ -9,4 +10,6 @@ data class Tag(
     override val id: Long? = null,
     override var createdAt: Instant? = null,
     override var updatedAt: Instant? = null,
+    @ManyToMany(mappedBy = "tags")
+    val kaomojis: Set<Kaomoji>? = null,
 ) : AbstractEntity()
